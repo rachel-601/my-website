cbdb6f9be16cb25e65fa6dc7144f5ad396d975d5
racheljune2004@gmail.com <racheljune2004@gmail.com>
2023-08-26 23:04:20 -0700

    fixed error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,5 @@
 "use client"
 import React, {useState} from "react"
-<<<<<<< HEAD
-import { BrowserRouter, Route, Link } from "react-router-dom";
-=======
->>>>>>> de4487b0b6a80e87385cb8131dd1d4bff63d061c
 
 function HomeButton() {
     const [active, set] = useState(false); 
@@ -15,7 +11,6 @@ function HomeButton() {
 
     function mouseEnterLeave() {
         set(!active)
-<<<<<<< HEAD
     }
 
     return (
@@ -24,82 +19,6 @@ function HomeButton() {
         onMouseEnter={mouseEnterLeave}
         onMouseLeave={mouseEnterLeave}>
             HOME
-=======
-    }
-
-    return (
-        <button className={active ? "text-purple-500": "text-black"} 
-        onClick={handleClick} 
-        onMouseEnter={mouseEnterLeave}
-        onMouseLeave={mouseEnterLeave}>
-            HOME
-        </button>
-    )
-}
-
-function AboutButton() {
-    const [active, set] = useState(false); 
-
-    function handleClick() {
-        set(false)
-        "change to Home page"
-    }
-
-    function mouseEnterLeave() {
-        set(!active)
-    }
-
-    return (
-        <button className={active ? "text-purple-500": "text-black"} 
-        onClick={handleClick} 
-        onMouseEnter={mouseEnterLeave}
-        onMouseLeave={mouseEnterLeave}>
-            ABOUT
-        </button>
-    )
-}
-
-function PortButton() {
-    const [active, set] = useState(false); 
-
-    function handleClick() {
-        set(false)
-        "change to Home page"
-    }
-
-    function mouseEnterLeave() {
-        set(!active)
-    }
-
-    return (
-        <button className={active ? "text-purple-500": "text-black"} 
-        onClick={handleClick} 
-        onMouseEnter={mouseEnterLeave}
-        onMouseLeave={mouseEnterLeave}>
-            PORTFOLIO
-        </button>
-    )
-}
-
-function ContactButton() {
-    const [active, set] = useState(false); 
-
-    function handleClick() {
-        set(false)
-        "change to Home page"
-    }
-
-    function mouseEnterLeave() {
-        set(!active)
-    }
-
-    return (
-        <button className={active ? "text-purple-500": "text-black"} 
-        onClick={handleClick} 
-        onMouseEnter={mouseEnterLeave}
-        onMouseLeave={mouseEnterLeave}>
-            CONTACT
->>>>>>> de4487b0b6a80e87385cb8131dd1d4bff63d061c
         </button>
     )
 }
