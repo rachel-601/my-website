6b5210327e9d4eb8587cf28ac03ecfb3c14eada8
racheljune2004@gmail.com <racheljune2004@gmail.com>
2023-08-26 23:02:55 -0700

    home page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,43 @@
 "use client"
 import React, {useState} from "react"
+<<<<<<< HEAD
 import { BrowserRouter, Route, Link } from "react-router-dom";
+=======
+>>>>>>> de4487b0b6a80e87385cb8131dd1d4bff63d061c
 
 function HomeButton() {
+    const [active, set] = useState(false); 
+
+    function handleClick() {
+        set(false)
+        "change to Home page"
+    }
+
+    function mouseEnterLeave() {
+        set(!active)
+<<<<<<< HEAD
+    }
+
+    return (
+        <button className={active ? "text-indigo-300": "text-black"} 
+        onClick={handleClick} 
+        onMouseEnter={mouseEnterLeave}
+        onMouseLeave={mouseEnterLeave}>
+            HOME
+=======
+    }
+
+    return (
+        <button className={active ? "text-purple-500": "text-black"} 
+        onClick={handleClick} 
+        onMouseEnter={mouseEnterLeave}
+        onMouseLeave={mouseEnterLeave}>
+            HOME
+        </button>
+    )
+}
+
+function AboutButton() {
     const [active, set] = useState(false); 
 
     function handleClick() {
@@ -15,11 +50,56 @@ function HomeButton() {
     }
 
     return (
-        <button className={active ? "text-indigo-300": "text-black"} 
+        <button className={active ? "text-purple-500": "text-black"} 
         onClick={handleClick} 
         onMouseEnter={mouseEnterLeave}
         onMouseLeave={mouseEnterLeave}>
-            HOME
+            ABOUT
+        </button>
+    )
+}
+
+function PortButton() {
+    const [active, set] = useState(false); 
+
+    function handleClick() {
+        set(false)
+        "change to Home page"
+    }
+
+    function mouseEnterLeave() {
+        set(!active)
+    }
+
+    return (
+        <button className={active ? "text-purple-500": "text-black"} 
+        onClick={handleClick} 
+        onMouseEnter={mouseEnterLeave}
+        onMouseLeave={mouseEnterLeave}>
+            PORTFOLIO
+        </button>
+    )
+}
+
+function ContactButton() {
+    const [active, set] = useState(false); 
+
+    function handleClick() {
+        set(false)
+        "change to Home page"
+    }
+
+    function mouseEnterLeave() {
+        set(!active)
+    }
+
+    return (
+        <button className={active ? "text-purple-500": "text-black"} 
+        onClick={handleClick} 
+        onMouseEnter={mouseEnterLeave}
+        onMouseLeave={mouseEnterLeave}>
+            CONTACT
+>>>>>>> de4487b0b6a80e87385cb8131dd1d4bff63d061c
         </button>
     )
 }
